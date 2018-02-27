633d1bc34f99cf5b186b4bc765e2c63adb515ee5
unokits <unokits@gmail.com>
2018-02-27 16:20:23 -0500

    Adding first .NET Core sample code

---
+++ HelloFibonacci/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Here are your numbers:");
+            var gen = new FibonacciGenerator();
+            foreach (var digit in gen.Generate(15)) {
+                Console.WriteLine(digit);
+            }
+        }
+    }
+}
